db56fc1ea845c2e31ef0ecc23c20545dc9165bd8
learn <learn@learn11.local>
2017-10-19 14:17:41 -0700

    first commit day 2 trouble with getting game pieces to show up

--- battleship.js
+++ battleship.js
@@ -1,16 +1,12 @@
+var tdArr = [];
 $(document).ready(function(){
   var headLetter = ["A","B","C","D","E","F","G","H","I","J"];
-  // var headLetterLower
-  // headLetter.forEach(function(element){
-  //     headLetterLower[element] = headLetter[element].toLowerCase();
-  // })
-  // var rowNumber = 0;
   var torpedoes = 0;
-  var board = [[], [], [], [], [], [], [], [], [], []]
+  // var board = [[], [], [], [], [], [], [], [], [], []]
   const ship = 1;
-  var lowerLetter = ["a","b","c","d","e","f","g","h","i","j"];
   var shipPos = [];
   var hits = 0;
+
 
   //populates ships on board
 
@@ -39,25 +35,42 @@ $(document).ready(function(){
 
     //clicking fires torpedoes and adds to torpedo counter
     $("td").on("click", function() {
-            if (hits <= 5 || torpedoes >= 25) {
+            if (hits <= 5 || torpedoes >= 10) {
             var thisSq = $(this).parent().attr("id") + $(this).attr("id");
             console.log(thisSq);
             if (shipPos.includes(thisSq)) {
                 $(this).addClass("hit");
+                //take this class and rename it then add below and reassign
                 hits++;
                 console.log("hits = " + hits);
             } else {
                 $(this).addClass("miss");
             }
-            if (torpedoes < 25) {
+            if (torpedoes < 10) {
             torpedoes += 1;
             $("#torpedoCount").text("Torpedoes used: " + torpedoes);
-            if (torpedoes >= 25 && hits < 5) {
+            if (torpedoes >= 10 && hits < 5) {
+
               $("#torpedoCount").text("You have run out of ammo, Game Over!");
+              $("td").off("click");
+              shipPos.forEach(function(el){
+                  var splitId = el.split("")
+                  console.log(splitId);
+                //   $("tr").
+                //   console.log($("table").children());
+
+                    // tdArr.push(($("tr").children()))
+                    // console.log($("table").children("") + $("tr").children());
+
+                //   .last("tr").attr("id") + $("tr").children().last("td").attr("id"));
+                //   console.log(splitId)
+                //   console.log(splitId[1])
+              })
+
 
           } else if (hits >= 5) {
               $("#torpedoCount").text("You Win!");
-
+              $("td").off("click");
           }
       }
 $(this).off("click");
